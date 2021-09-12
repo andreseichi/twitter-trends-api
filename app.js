729f8827d7fa6a775baf7c388f33b26e7b7b7663
André Tashiro <andre_seichi@hotmail.com>
2021-09-12 16:14:25 -0300

    consuming the api

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
+require('dotenv').config();
 const express = require('express');
+
+const twitterApi = require('./api/twitter');
 
 const app = express();
 const port = process.env.PORT || 3030;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to my application 😘');
+  res.send('Welcome to my application 😃');
+});
+
+// global trends
+app.get('/trends', async (req, res) => {
+  try {
+    const { data } = await twitterApi.get('trends/place.json?id=1');
+    return res.send(data);
+  } catch (error) {
+    res.send({ error: error.message });
+  }
+});
+
+// trends woeid based
+app.get('/trends/:id', async (req, res) => {
+  console.log(req.params.id);
+  const { id } = req.params;
+
+  try {
+    const { data } = await api.get(`trends/place.json?id=${id}`);
+    return res.send(data);
+  } catch (error) {
+    res.send({ error: error.message });
+  }
 });
 
 app.listen(port, () => {
